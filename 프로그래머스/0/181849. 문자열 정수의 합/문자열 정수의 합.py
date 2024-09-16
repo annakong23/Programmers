6b2d59bbbcc1775395058bd18aec05fd0bbf2821
annakong23 <102771961+annakong23@users.noreply.github.com>
2024-09-16 17:56:10 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -0,0 +1,11 @@
+def solution(num_str):
+    num_list = []
+    answer = sum(int(i) for i in num_str)
+    return answer
+
+# def solution(num_str):
+#     num_list = []
+#     for i in num_str:
+#         num_list.append(int(i))
+#     answer = sum(num_list)
+#     return answer
