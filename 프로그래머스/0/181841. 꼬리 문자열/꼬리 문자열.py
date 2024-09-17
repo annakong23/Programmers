d3db8762764e0b3e52d8063d1856b9c6d1d33a25
annakong23 <102771961+annakong23@users.noreply.github.com>
2024-09-17 18:16:25 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.py
@@ -0,0 +1,11 @@
+# def solution(str_list, ex):
+#     answer = []
+#     for word in str_list:
+#         if ex not in word:
+#             answer.append(word)
+#     answer = ''.join(answer)
+#     print(answer)
+#     return answer
+
+def solution(str_list, ex):
+    return ''.join(filter(lambda x: ex not in x ,str_list))
