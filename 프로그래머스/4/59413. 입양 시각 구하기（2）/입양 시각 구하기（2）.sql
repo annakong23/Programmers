cb43e4c8d0f48e695fce12bcec749fa383130ebb
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-07-20 01:17:21 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,13 +1,10 @@
-# set @HOUR = -1;
-# select (@HOUR := @HOUR + 1) HOUR,(select count(hour(datetime)) HOUR
-# from animal_outs
-# where hour(datetime)=@hour) COUNT
-# from animal_outs
-# where @HOUR < 23;
-
-
 set @HOUR = -1;
 select (@HOUR := @HOUR+1) HOUR, (select count(*) from animal_outs where hour(datetime)=@HOUR) COUNT
 from animal_outs
 where @hour<23
 order by hour;
+
+# select hour(datetime) HOUR, count(animal_id) COUNT
+# from animal_outs
+# group by hour
+# order by hour
