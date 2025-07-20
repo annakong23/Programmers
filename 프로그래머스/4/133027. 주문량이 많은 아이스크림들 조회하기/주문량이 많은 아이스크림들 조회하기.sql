f764cdff043498cefff6b2549c86d01c641445c0
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-07-20 17:58:21 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,10 +1,30 @@
-select a.FLAVOR
-from
-((select *
-from first_half)
-union all
-(select *
-from july)) as a
-group by flavor
-order by sum(total_order) desc
+# select a.FLAVOR
+# from
+# ((select *
+# from first_half)
+# union all
+# (select *
+# from july)) as a
+# group by flavor
+# order by sum(total_order) desc
+# limit 3
+
+
+
+select j.flavor
+from first_half h
+join
+(select flavor, sum(total_order) total_order
+from july
+group by flavor) j on h.flavor=j.flavor
+group by j.flavor
+order by (h.total_order+j.total_order) desc
 limit 3
+
+
+
+
+
+
+
+
