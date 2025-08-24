163b76d530d7d73031119ad50aa7ac85b1ac983a
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-08-24 14:40:06 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,25 @@
 def solution(array, commands):
-    answer = []
-    for a in range(len(commands)):
-        i,j,k=commands[a][0], commands[a][1], commands[a][2]
-        answer.append(sorted(array[i-1:j])[k-1])
-    return answer
+    num_list=[]
+    for i in range(0,len(commands)):
+        num_list.append(sorted(array[commands[i][0]-1:commands[i][1]])[commands[i][2]-1])
+    return num_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def solution(array, commands):
+#     answer = []
+#     for a in range(len(commands)):
+#         i,j,k=commands[a][0], commands[a][1], commands[a][2]
+#         answer.append(sorted(array[i-1:j])[k-1])
+#     return answer
