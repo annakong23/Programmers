04950a54a9d497aba2e25873c5f847b269d53fc1
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-08-28 00:48:28 +0900

    [level 0] Title: 부분 문자열, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.py
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.py
@@ -1,3 +1,6 @@
+# def solution(str1, str2):
+#     answer = 1 if str1 in str2 else 0
+#     return answer
+
 def solution(str1, str2):
-    answer = 1 if str1 in str2 else 0
-    return answer
+    return 1 if str1 in str2 else 0
