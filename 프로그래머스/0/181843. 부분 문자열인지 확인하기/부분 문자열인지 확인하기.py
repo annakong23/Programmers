4dbbf71ae8590dc9bf45d3ce6a2d3c8639dc0913
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-08-28 00:50:30 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.py
@@ -1,3 +1,16 @@
 def solution(my_string, target):
-    answer = 1 if target in my_string else 0
-    return answer
+    return 1 if target in my_string else 0
+
+
+
+
+
+
+
+
+
+
+
+# def solution(my_string, target):
+#     answer = 1 if target in my_string else 0
+#     return answer
