3bffe9168d2eada89fa229fc2e09cb31cae88c9d
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-08-28 11:45:32 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.14 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,3 +1,7 @@
+# def solution(myString, pat):
+#     answer = 1 if pat.lower() in myString.lower() else 0
+#     return answer
+
+
 def solution(myString, pat):
-    answer = 1 if pat.lower() in myString.lower() else 0
-    return answer
+    return 1 if pat.lower() in myString.lower() else 0
