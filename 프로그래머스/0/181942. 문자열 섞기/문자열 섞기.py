12e25dff7c6007b25a1144802190a45dbe288118
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-08-28 14:01:37 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,6 +1,12 @@
+# def solution(str1, str2):
+#     answer = []
+#     for i,s in enumerate(str1):
+#         answer.append(s)
+#         answer.append(str2[i])
+#     return ''.join(answer)
+
 def solution(str1, str2):
-    answer = []
-    for i,s in enumerate(str1):
-        answer.append(s)
-        answer.append(str2[i])
-    return ''.join(answer)
+    answer = ''
+    for i in range(len(str1)):
+        answer += str1[i] + str2[i] 
+    return answer
