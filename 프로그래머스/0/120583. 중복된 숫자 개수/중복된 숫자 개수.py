c6eb7165f612f13f3e7b1907ee435b43d8c24b5e
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-09-01 18:44:31 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -1,2 +1,5 @@
+# def solution(array, n):
+#     return sum(array[i]==n for i in range(len(array)))
+
 def solution(array, n):
-    return sum(array[i]==n for i in range(len(array)))
+    return array.count(n)
