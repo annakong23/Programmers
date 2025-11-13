b68c67b39281789b1e5b9167ea9002b9ae9d43ee
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2025-11-13 17:23:10 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,4 +1,23 @@
 -- 코드를 입력하세요
-SELECT name
+# SELECT name
+# from animal_ins
+# where datetime = (select min(datetime) from animal_ins)
+
+# SELECT NAME
+# FROM ANIMAL_INS
+# ORDER BY DATETIME
+# LIMIT 1
+
+select NAME
 from animal_ins
-where datetime = (select min(datetime) from animal_ins)
+order by datetime
+limit 1
+
+
+
+
+
+
+
+
+
