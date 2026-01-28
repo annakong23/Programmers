07b7039df2991d4a78f71d256c436034648e4bca
Jiwon Kong <102771961+annakong23@users.noreply.github.com>
2026-01-28 15:54:02 +0900

    [level 1] Title: 최소직사각형, Time: 7.41 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,3 +1,21 @@
+# def solution(sizes):
+#     lst = [sorted(x) for x in sizes]
+#     return max(x[0] for x in lst) * max(x[1] for x in lst)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def solution(sizes):
     sizes = [sorted(x) for x in sizes]
     max_values = [max(x) for x in zip(*sizes)]
